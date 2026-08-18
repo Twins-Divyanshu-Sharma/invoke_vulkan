84419994ee2248da156ee444d98662abc68a8a2b
Divyanshu Sharma <twins-divyanshu-sharma@github.com>
2026-08-18 15:58:33 +0530

    let the vulkan commence

---
+++ include/VulkanCommence.hpp
@@ -0,0 +1,34 @@
+#ifndef H_VULKAN_COMMENCE_H
+#define H_VULKAN_COMMENCE_H
+
+#include "vk_mem_alloc.h"
+#include <iostream>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.hpp>
+#include <string>
+
+class VulkanCommence
+{
+
+private: 
+  VulkanCommence();
+  ~VulkanCommence() = default;
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback (
+      VkDebugUtilsMessageSeverityFlagBitsEXT,
+      VkDebugUtilsMessageTypeFlagsEXT,
+      const VkDebugUtilsMessengerCallbackDataEXT *,
+      void *pUserData
+  );
+
+public: 
+   VulkanCommence(const VulkanCommence&) = delete;
+   VulkanCommence& operator=(const VulkanCommence&) = delete;
+   VulkanCommence(VulkanCommence&&) = delete;
+   VulkanCommence& operator=(VulkanCommence&&) = delete;
+  
+   static VulkanCommence& getTheOnlyObject();
+};
+
+#endif
