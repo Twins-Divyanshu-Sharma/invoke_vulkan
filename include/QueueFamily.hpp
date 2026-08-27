da052311662df87abb9899a6e2370b8ba92622b8
Divyanshu Sharma <twins-divyanshu-sharma@github.com>
2026-08-27 18:45:56 +0530

    QueueFamilies

---
+++ include/QueueFamily.hpp
@@ -0,0 +1,19 @@
+#ifndef H_QUEUE_FAMILY_H
+#define H_QUEUE_FAMILY_H
+
+#include <optional>
+#include <vulkan/vulkan.hpp>
+
+namespace invoker::setup_vulkan
+{
+   struct QueueFamilyIndicies {
+        std::optional<uint32_t> graphicsFamily;
+        bool isComplete(); 
+    };
+  
+    QueueFamilyIndicies findQueueFamilies(VkPhysicalDevice device);
+    bool isDeviceSuitable(VkPhysicalDevice);
+}
+
+
+#endif
---
+++ source/QueueFamily.cpp
@@ -0,0 +1,43 @@
+#include "QueueFamily.hpp"
+
+namespace invoker::setup_vulkan
+{
+
+    bool QueueFamilyIndicies::isComplete()
+    {
+        return graphicsFamily.has_value();
+
+    }
+
+    QueueFamilyIndicies findQueueFamilies(VkPhysicalDevice device)
+    {
+        QueueFamilyIndicies indices;
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+       
+        int i = 0;
+        for(const auto& queueFamily : queueFamilies)
+        {
+            if(queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+                indices.graphicsFamily = i;
+
+            if(indices.isComplete())
+                break;
+
+            i++;
+        }
+
+        return indices;
+    }
+
+    bool isDeviceSuitable(VkPhysicalDevice device)
+    {
+        QueueFamilyIndicies indices = findQueueFamilies(device);
+        return indices.isComplete();
+    }
+
+}
+
+
